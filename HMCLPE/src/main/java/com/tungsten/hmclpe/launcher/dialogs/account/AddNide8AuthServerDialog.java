3594ba6331ca5030f9df6d4c9f66f4d1b64d8e01
root-S7 <3020825808@qq.com>
2023-01-25 22:07:20 +0800

    统一通行证(nide8auth)地址更改

--- HMCLPE/src/main/java/com/tungsten/hmclpe/launcher/dialogs/account/AddNide8AuthServerDialog.java
+++ HMCLPE/src/main/java/com/tungsten/hmclpe/launcher/dialogs/account/AddNide8AuthServerDialog.java
@@ -40,7 +40,7 @@ public class AddNide8AuthServerDialog extends Dialog implements View.OnClickList
 
     private AuthlibInjectorServer authlibInjectorServer;
 
-    public static final String NIDE_8_AUTH_SERVER = "https://auth.mc-user.com:233/";
+    public static final String NIDE_8_AUTH_SERVER = "https://login.mc-user.com/";
 
     public AddNide8AuthServerDialog(@NonNull Context context,OnNide8AuthServerAddListener onNide8AuthServerAddListener) {
         super(context);
